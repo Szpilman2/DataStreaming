af245ecd6c017210f1d24bd6636f9abca19abd81
Peyman Shabani <peymanshabani1998@gmail.com>
2025-05-06 14:20:19 +0330

    add worker pool to stream handler to control number of concurrent external api calls

--- Adapter/HttpAdapter.go
+++ Adapter/HttpAdapter.go
@@ -13,28 +13,34 @@ type HttpAdapter interface {
 }
 
 type StreamingAdapter struct {
-	ExternalAPI string
-	Client      *http.Client
-	NumWorkers  int
-	NumRequests int
+	ExternalAPI   string
+	Client        *http.Client
+	NumWorkers    int
+	NumRequests   int
+	Job           struct{ Index int }
+	CollectedStreamData struct {
+		Index int
+		Data  string
+	}
 }
 
 func NewStreamingAdapter(ExternalAPI string) *StreamingAdapter {
+	// Timeout: 30 * time.Second
 	return &StreamingAdapter{
 		ExternalAPI: ExternalAPI,
-		Client:      &http.Client{Timeout: 30 * time.Second},
-		NumWorkers:  1,
+		Client:      &http.Client{},
+		NumWorkers:  4,
 		NumRequests: 20,
 	}
 }
 
-func (s StreamingAdapter) SetHeaders(w http.ResponseWriter) {
+func (s *StreamingAdapter) SetHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 }
 
-func (s StreamingAdapter) CheckStreamingSupport(w http.ResponseWriter) (http.Flusher, error) {
+func (s *StreamingAdapter) CheckStreamingSupport(w http.ResponseWriter) (http.Flusher, error) {
 	flusher, ok := w.(http.Flusher)
 	if !ok {
 		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
@@ -43,7 +49,7 @@ func (s StreamingAdapter) CheckStreamingSupport(w http.ResponseWriter) (http.Flu
 	return flusher, nil
 }
 
-func (s StreamingAdapter) HandleExternalAPICall(w http.ResponseWriter, r *http.Request) {
+func (s *StreamingAdapter) HandleExternalAPICall(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("New stream client connected")
 	s.SetHeaders(w)
 
@@ -55,7 +61,6 @@ func (s StreamingAdapter) HandleExternalAPICall(w http.ResponseWriter, r *http.R
 
 	ticker := time.NewTicker(10 * time.Second)
 	done := make(chan struct{})
-
 	go func() {
 		for {
 			select {
@@ -68,18 +73,51 @@ func (s StreamingAdapter) HandleExternalAPICall(w http.ResponseWriter, r *http.R
 		}
 	}()
 
-	for i := 0; i < s.NumRequests; i++ {
-		url := fmt.Sprintf("%s%d", s.ExternalAPI, i)
-		resp, err := s.Client.Get(url)
-		if err != nil {
-			fmt.Fprintf(w, "data: Error %d: %v\n\n", i, err)
-			flusher.Flush()
-			continue
-		}
-		body, _ := io.ReadAll(resp.Body)
-		resp.Body.Close()
+	// Channels
+	jobs := make(chan struct{ Index int }, s.NumRequests)
+	CollectedStreamDatas := make(chan struct {
+		Index int
+		Data  string
+	}, s.NumRequests)
 
-		fmt.Fprintf(w, "data: %s\n\n", string(body))
+	// Workers: Process jobs concurrently, but stream results one by one
+	for w := 0; w < s.NumWorkers; w++ {
+		go func() {
+			for job := range jobs {
+				url := fmt.Sprintf("%s%d", s.ExternalAPI, job.Index)
+				resp, err := s.Client.Get(url)
+				var data string
+				if err != nil {
+					data = fmt.Sprintf("Error %d: %v", job.Index, err)
+				} else {
+					body, _ := io.ReadAll(resp.Body)
+					resp.Body.Close()
+					data = string(body)
+				}
+				// Send the result to the channel for streaming
+				CollectedStreamDatas <- struct {
+					Index int
+					Data  string
+				}{Index: job.Index, Data: data}
+			}
+		}()
+	}
+
+	// Send jobs to workers
+	go func() {
+		for i := 0; i < s.NumRequests; i++ {
+			jobs <- struct{ Index int }{Index: i}
+		}
+		close(jobs)
+	}()
+
+	// Collect and stream data in order
+	for i := 0; i < s.NumRequests; i++ {
+		// Wait for the next result from the workers
+		CollectedStreamData := <-CollectedStreamDatas
+
+		// Stream the collected data one by one
+		fmt.Fprintf(w, "data: %s\n\n", CollectedStreamData.Data)
 		flusher.Flush()
 	}
 
@@ -108,19 +146,19 @@ func (p PlainAdapter) SetHeaders(w http.ResponseWriter) {
 func (p PlainAdapter) HandleExternalAPICall(w http.ResponseWriter, r *http.Request) {
 	p.SetHeaders(w)
 
-	var results []string
+	var CollectedStreamDatas []string
 	for i := 0; i < p.NumRequests; i++ {
 		url := fmt.Sprintf("%s%d", p.ExternalAPI, i)
 		resp, err := p.Client.Get(url)
 		if err != nil {
-			results = append(results, fmt.Sprintf("Error %d: %v", i, err))
+			CollectedStreamDatas = append(CollectedStreamDatas, fmt.Sprintf("Error %d: %v", i, err))
 			continue
 		}
 		body, _ := io.ReadAll(resp.Body)
 		resp.Body.Close()
 
-		results = append(results, string(body))
+		CollectedStreamDatas = append(CollectedStreamDatas, string(body))
 	}
 
-	fmt.Fprint(w, "[\""+fmt.Sprint(results)+"\"]")
+	fmt.Fprint(w, "[\""+fmt.Sprint(CollectedStreamDatas)+"\"]")
 }
